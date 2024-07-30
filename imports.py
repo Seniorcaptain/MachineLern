f87fcea967de11fbcb49ac3ddadcfbbff8f1c7a6
Seniorcaptain <36276653+Seniorcaptain@users.noreply.github.com>
2024-07-30 19:34:39 +0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,77 +1,66 @@
-from __future__ import print_function
 import cv2 as cv
-import argparse
-import matplotlib as plt
-from tkinter import *
-from tkinter import filedialog
+import numpy as np
+import matplotlib.pyplot as plt
+from tkinter import Tk, filedialog
 
-root = Tk()
+def template_matching(image_path, template_path):
+    img = cv.imread(image_path, 0)
+    template = cv.imread(template_path, 0)
+    w, h = template.shape[::-1]
 
-filename = filedialog.askdirectory(initialdir='D://Music')
-template = cv.imread('template.jpg', 0)
-w, h = template.shape[::-1]
+    methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+               'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
 
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-           'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-for meth in methods:
+    for meth in methods:
+        method = eval(meth)
+        res = cv.matchTemplate(img, template, method)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
-    method = eval(meth)
+        top_left = min_loc if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED] else max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
 
-    # Apply template Matching
+        cv.rectangle(img, top_left, bottom_right, 255, 2)
 
-    res = cv.matchTemplate(filename, template, method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+        plt.subplot(121), plt.imshow(res, cmap='gray')
+        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122), plt.imshow(img, cmap='gray')
+        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+        plt.suptitle(meth)
+        plt.show()
 
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+def threshold_demo():
+    max_value = 255
+    max_type = 4
+    window_name = 'Threshold Demo'
 
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(filename, top_left, bottom_right, 255, 2)
-    plt.subplot(121), plt.imshow(res, cmap='gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(filename, cmap='gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
-max_value = 255
-max_type = 4
-max_binary_value = 255
-trackbar_type = 'Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted'
-trackbar_value = 'Value'
-window_name = 'Threshold Demo'
+    def on_trackbar(val):
+        threshold_type = cv.getTrackbarPos('Type', window_name)
+        threshold_value = cv.getTrackbarPos('Value', window_name)
+        _, dst = cv.threshold(src_gray, threshold_value, max_value, threshold_type)
+        cv.imshow(window_name, dst)
 
+    src = cv.imread('template.jpg')
+    if src is None:
+        print('Could not open or find the image')
+        return
 
-def Threshold_Demo(val):
-    # 0: Binary
-    # 1: Binary Inverted
-    # 2: Threshold Truncated
-    # 3: Threshold to Zero
-    # 4: Threshold to Zero Inverted
-    threshold_type = cv.getTrackbarPos(trackbar_type, window_name)
-    threshold_value = cv.getTrackbarPos(trackbar_value, window_name)
-    _, dst = cv.threshold(src_gray, threshold_value, max_binary_value, threshold_type)
-    cv.imshow(window_name, dst)
+    src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    cv.namedWindow(window_name)
+    cv.createTrackbar('Type', window_name, 3, max_type, on_trackbar)
+    cv.createTrackbar('Value', window_name, 0, max_value, on_trackbar)
 
+    on_trackbar(0)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-parser = argparse.ArgumentParser(description='Code for Basic Thresholding Operations tutorial.')
-parser.add_argument('--input', help='Path to input image.', default='stuff.jpg')
+if __name__ == '__main__':
+    root = Tk()
+    root.withdraw()  # Hide the main window
 
-args = parser.parse_args()
-src = cv.imread(template)
-if src is None:
-    print('Could not open or find the image: ', args.input)
-    exit(0)
-# Convert the image to Gray
-src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-cv.namedWindow(window_name)
-cv.createTrackbar(trackbar_type, window_name, 3, max_type, Threshold_Demo)
-# Create Trackbar to choose Threshold value
-cv.createTrackbar(trackbar_value, window_name, 0, max_value, Threshold_Demo)
-# Call the function to initialize
-Threshold_Demo(9)
-# Wait until user finishes program
-cv.waitKey()
-root.mainloop()
+    image_path = filedialog.askopenfilename(initialdir='D://Music', title="Select Image")
+    template_path = 'template.jpg'
+
+    if image_path:
+        template_matching(image_path, template_path)
+    
+    threshold_demo()
